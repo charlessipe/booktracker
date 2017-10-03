c10d51e77235bfbab8d979c791a174b399b1337a
Charles Sipe <csipe84@gmail.com>
2017-10-03 07:46:19 -0700

    change state with book component on wanttoread

--- src/book.js
+++ src/book.js
@@ -4,10 +4,20 @@ import './App.css'
 import * as BooksAPI from './BooksAPI';
 
 class Book extends React.Component {
-  state = {
-    currentlyReading: "false",
-    wantToRead: "false",
-    read: "false"
+
+  constructor() {
+    super();
+    this.state = {
+      currentlyReading: "false",
+      wantToRead: "false",
+      read: "false",
+      selectedBookState: "alreadyreadit"
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.props.setState({selectedBookState: event.target.value});
   }
 
   render() {
@@ -18,7 +28,7 @@ class Book extends React.Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 174, backgroundImage: 'url("http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api")' }}></div>
           <div className="book-shelf-changer">
-            <select value={this.state.selectedBookState} onChange={this.handleChange}>
+            <select value={this.state.selectedBookState} onChange={this.props.handleChange}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
--- src/wanttoreadsoon.js
+++ src/wanttoreadsoon.js
@@ -9,7 +9,7 @@ class WantToRead extends React.Component {
     this.state = {
       selectedBookState: "wanttoreadsoon"
     }
-    
+
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -25,27 +25,9 @@ class WantToRead extends React.Component {
         <h2 className="bookshelf-title">Want to Read Soon {this.state.selectedBookState}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select value={this.state.selectedBookState} onChange={this.handleChange}>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-              </div>
-            </li>
-            <li><Book title="Ready Player One" author="Dan Ann"/></li>
-            <li><Book title="1776" author="David McCullough"/></li>
-            <li><Book title="Harry Potter and the Sorcerers Stone" author="JK Rowling"/></li>
+            <li><Book title="Ready Player One" author="Dan Ann" handleChange={this.handleChange}/></li>
+            <li><Book title="1776" author="David McCullough" handleChange={this.handleChange}/></li>
+            <li><Book title="Harry Potter and the Sorcerers Stone" author="JK Rowling" handleChange={this.handleChange}/></li>
           </ol>
         </div>
       </div>
